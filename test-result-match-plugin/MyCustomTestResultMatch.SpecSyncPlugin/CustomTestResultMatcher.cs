1a6b05017b802763d021104e88c64c6c37d081d6
Gaspar Nagy <gaspar.nagy@gmail.com>
2021-02-26 08:10:43 +0100

    create special matcher plugin

--- test-result-match-plugin/MyCustomTestResultMatch.SpecSyncPlugin/CustomTestResultMatcher.cs
+++ test-result-match-plugin/MyCustomTestResultMatch.SpecSyncPlugin/CustomTestResultMatcher.cs
@@ -10,9 +10,7 @@ namespace MyCustomTestResultMatch.SpecSyncPlugin
 
         public override bool CanProcess(TestRunnerResultMatcherArgs args)
         {
-            // for using this matcher only for a specific test runner, you can use the args.TestFrameworkIdentifier, like:
-            // return args.TestFrameworkIdentifier.IndexOf("mstest", StringComparison.InvariantCultureIgnoreCase) >= 0;
-            return true;
+            return args.TestFrameworkIdentifier == "executor://nunit3testexecutor/";
         }
 
         protected override MatchResultSelector GetScenarioResultSelector(ScenarioLocalTestCase scenarioLocalTestCase, FeatureFileLocalTestCaseContainer featureFileLocalTestCaseContainer, TestRunnerResultMatcherArgs args)
@@ -22,14 +20,13 @@ namespace MyCustomTestResultMatch.SpecSyncPlugin
 
             // Use scenarioLocalTestCase.IsScenarioOutline is matching needs to be done differently 
             // for scenario outlines.
-
+            
             // The first parameter of the MatchResultSelector is a diagnostic message that helps users to 
             // understand why the matcher could not find a test.
             // To see these messages, invoke SpecSync with an additional --verbose option.
 
-            return new MatchResultSelector($"<className> ends with '{featureName}' and <name> is '{scenarioName}'",
-                td => td.ClassName.EndsWith(featureName) &&
-                      td.Name == scenarioName);
+            return new MatchResultSelector($"<name> starts with '{scenarioName}'",
+                td => td.Name.StartsWith(scenarioName));
         }
     }
 }
