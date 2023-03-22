527e05a7536c110b3fab281016932ece4fbeb14e
Gaspar Nagy <gaspar.nagy@gmail.com>
2023-03-22 12:05:17 +0100

    plugin parameters

---
+++ postman-test-source-plugin/SpecSync.Plugin.PostmanTestSource/PluginParameterExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using SpecSync.Plugins;
+
+namespace SpecSync.Plugin.PostmanTestSource;
+
+public static class PluginParameterExtensions
+{
+    public static TParameters GetParametersAs<TParameters>(this PluginInitializeArgs args)
+    {
+        var paramJObject = JObject.FromObject(args.Parameters);
+        return paramJObject.ToObject<TParameters>();
+    }
+}
